8b2847999dbacb3e84af8caa74155bc3782958a5
Jason Scott <becomingcoder78@gmail.com>
2016-04-14 20:09:54 -0400

    Updated Movie lab

--- src/movieList/Movie.java
+++ src/movieList/Movie.java
@@ -1,5 +1,29 @@
 package movieList;
 
 public class Movie {
+	
+	private String Title;
+	
+	private String Category;
 
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getCategory() {
+		return Category;
+	}
+
+	public void setCategory(String cat) {
+		Category = cat;
+	}
+
+	public void printMovie() {
+		System.out.println(getTitle());
+	}
+	
 }
--- src/movieList/MovieDatabase.java
+++ src/movieList/MovieDatabase.java
@@ -1,10 +1,34 @@
 package movieList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
 public class MovieDatabase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		ArrayList<Movie> myMovies = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+        String[] title={"SW1","SW2","SW3","Bob"};
+        String[] cat={"Scifi","Scifi","Scifi","Drama"};
+        
+        for (int i = 0; i < 4; i++) {
+            myMovies.add(new Movie());
+            
+            myMovies.get(i).setTitle(title[i]);
+            myMovies.get(i).setCategory(cat[i]);
+            
+    		for (Movie p:myMovies) { 
+    			p.printMovie();
+    		}
+            
+        }
 
+       
 	}
 
 }
